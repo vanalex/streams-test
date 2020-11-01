3966ad3b14a2985abe7fe28d9a20a4e2b8482e42
alejandropajaronjorquera <a.pajaron.jorquera@dkbcodefactory.com>
2020-11-01 19:00:05 +0100

    add more examples on creation streams tests.

--- src/test/java/com/examples/streams/StreamexTest.java
+++ src/test/java/com/examples/streams/StreamexTest.java
@@ -3,6 +3,9 @@ package com.examples.streams;
 import one.util.streamex.StreamEx;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.Reader;
+import java.io.StringReader;
 import java.util.Optional;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
@@ -28,5 +31,17 @@ public class StreamexTest {
         assertThat(asList("a", "b")).isEqualTo(StreamEx.split("a,b", ",").toList());
         assertThat(asList("a", "c", "d")).isEqualTo(StreamEx.split("abcBd", Pattern.compile("b", Pattern.CASE_INSENSITIVE))
                 .toList());
+        assertThat(asList("a", "b")).isEqualTo(StreamEx.ofLines(new StringReader("a\nb")).toList());
+        assertThat(asList("a", "b")).isEqualTo(StreamEx.ofLines(new BufferedReader(new StringReader("a\nb"))).toList());
+        assertThat(asList("a", "b")).isEqualTo(StreamEx.ofLines(getReader()).toList());
+        assertThat(asList("a", "a", "a", "a")).isEqualTo(StreamEx.generate(() -> "a").limit(4).toList());
+        assertThat(asList("a", "a", "a", "a")).isEqualTo( StreamEx.constant("a", 4).toList());
+        assertThat(asList("c", "d", "e")).isEqualTo(StreamEx.of("abcdef".split(""), 2, 5).toList());
+
+
+    }
+
+    private static Reader getReader() {
+        return new BufferedReader(new StringReader("a\nb"));
     }
 }
